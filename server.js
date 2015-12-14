d0db9cde325a50433ac26307fc54777294701c61
Declan Elcocks <declan.elcocks@gmail.com>
2015-12-14 17:18:33 +0800

    some commit

--- server.js
+++ server.js
@@ -1,28 +1,32 @@
-var http = require('http'),
-  express = require('express'),
-  bodyParser = require('body-parser'),
-  faye = require('faye');
-
-var bayeux = new faye.NodeAdapter({
-  mount: '/faye',
-  timeout: 45
-});
-
-var app = express();
-var server = http.createServer(app);
-bayeux.attach(server);
+var http          = require('http'),
+    express       = require('express'),
+    bodyParser    = require('body-parser'),
+    faye          = require('faye'),
+    app           = express(),
+    server        = http.createServer(app);
 
 app.use(bodyParser.urlencoded({
   extended: false
 }));
 app.use(express['static'](__dirname + '/public'));
 
+var ws = new faye.NodeAdapter({
+  mount: '/faye',
+  timeout: 45
+});
+
+ws.attach(server);
+
 app.post('/message', function(req, res) {
-  bayeux.getClient().publish('/channel', {
+  ws.getClient().publish('/channel', {
     text: req.body.message
   });
   console.log('broadcast message:' + req.body.message);
   res.send(200);
+});
+
+app.get('/', function(req, res) {
+  res.send(200).status({message: 'hi mate'});
 });
 
 server.listen(8181);
